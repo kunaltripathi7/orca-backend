b4018c5e2b47f0b27abce74e7aeaaf00b6cf44e4
kunaltripathi7 <ikunaltripathi@gmail.com>
2024-09-16 10:21:27 +0530

    updated-script

--- src/controllers/channelController.ts
+++ src/controllers/channelController.ts
@@ -29,15 +29,16 @@ export const createChannel = async (
       )
     );
 
-  if (!name || !type)
+  if (!name || !type) {
+    const message = !name ? "Name is missing" : "Type is missing";
     return next(
       new UnprocessbleEntity(
         new Error(),
-        "Name/type is Missing",
+        message,
         ErrorCode.UNPROCESSABLE_ENTITY
       )
     );
-
+  }
   if (name.toLowerCase() === "general" || name.length > 10)
     return next(
       new UnprocessbleEntity(
@@ -65,6 +66,59 @@ export const createChannel = async (
           profileId: profile.id,
           name,
           type,
+        },
+      },
+    },
+  });
+
+  res.json(server);
+};
+
+export const deleteChannel = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const profile = await currentProfile(req);
+  const serverId = req.query.serverId?.toString() ?? undefined;
+  const { channelId } = req.params;
+  if (!profile)
+    return next(
+      new NotFoundException("User Profile Not found", ErrorCode.USER_NOT_FOUND)
+    );
+
+  if (!serverId || !channelId) {
+    const id = !serverId ? "serverId" : "channelId";
+    return next(
+      new UnprocessbleEntity(
+        new Error(),
+        `${id} is Missing`,
+        ErrorCode.UNPROCESSABLE_ENTITY
+      )
+    );
+  }
+
+  console.log(channelId, serverId);
+
+  const server = await db.server.update({
+    where: {
+      id: serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+          role: {
+            in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+          },
+        },
+      },
+    },
+    data: {
+      channels: {
+        delete: {
+          id: channelId,
+          name: {
+            not: "general",
+          },
         },
       },
     },
--- src/routes/channel.ts
+++ src/routes/channel.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { errorHandler } from "../errorHandler";
-import { createChannel } from "../controllers/channelController";
+import { createChannel, deleteChannel } from "../controllers/channelController";
 import multer from "multer";
 
 const channelRoutes: Router = Router();
 const upload = multer();
 
 channelRoutes.post("/", upload.none(), errorHandler(createChannel));
+channelRoutes.delete("/:channelId", errorHandler(deleteChannel));
 
 export default channelRoutes;
